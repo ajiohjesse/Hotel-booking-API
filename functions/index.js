3b1a3c36f99dfeae652a89db05433710f35a75e8
Jesse Ajioh <ajiohjesse@gmail.com>
2023-03-08 10:45:59 +0100

    add authentication

--- functions/index.js
+++ functions/index.js
@@ -8,6 +8,7 @@ const usersRoute = require('../src/routes/users.js');
 const hotelsRoute = require('../src/routes/hotels.js');
 const roomsRoute = require('../src/routes/rooms.js');
 const dbConnect = require('../src/lib/mongoose.js');
+const createError = require('../src/lib/error.js');
 
 const app = express();
 
@@ -19,6 +20,21 @@ dbConnect().catch(err => console.log(err));
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
+
+app.use((req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token || token !== process.env.BEARER_TOKEN) {
+    return next(createError(401, 'Not Authorized'));
+  }
+
+  next();
+});
+
+app.use('/api/auth', authRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/hotels', hotelsRoute);
+app.use('/api/rooms', roomsRoute);
 
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
@@ -32,10 +48,5 @@ app.use((err, req, res, next) => {
     stack: err.stack,
   });
 });
-
-app.use('/api/auth', authRoute);
-app.use('/api/users', usersRoute);
-app.use('/api/hotels', hotelsRoute);
-app.use('/api/rooms', roomsRoute);
 
 module.exports.handler = ServerlessHttp(app);
